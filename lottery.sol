0766efbbc0238ee6a522c3a7b514c4637ccc4ca2
Onkar Kumar <63390852+onkar4580@users.noreply.github.com>
2022-02-07 21:36:06 +0530

    Update lottery.sol

--- lottery.sol
+++ lottery.sol
@@ -2,12 +2,6 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Lottery  {
-
-    // SPDX-License-Identifier: GPL-3.0
-
-pragma solidity >=0.7.0 <0.9.0;
-
 contract Lottery 
 {
     address payable[] public participants;
